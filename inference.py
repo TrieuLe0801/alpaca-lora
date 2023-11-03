57b425bc72ef1abd5e2beee54022faf5c8a31588
trieulv <trieulv@ftech.ai>
2023-11-03 17:20:07 +0000

    refactor: set fire for arguments setting

--- inference.py
+++ inference.py
@@ -183,12 +183,12 @@ def main(
 
 
 if __name__ == "__main__":
-    input_text_list = convert_text_to_list()
-    # fire.Fire(main)
-    output_list = main(
-        input_text=input_text_list[0],
-        instruction="Given a medical context and a multiple choice question related to it, select the correct answers from the four options. The answer is binary string with length is the number of opitons, 1 is correct and 0 is incorrect",
-        load_8bit=True,
-        base_model="medalpaca/medalpaca-7b",
-        lora_weights="./lora-alpaca/checkpoint-800",
-    )
+    # input_text_list = convert_text_to_list()
+    fire.Fire(main)
+    # output_list = main(
+    #     input_text=input_text_list[0],
+    #     instruction="Given a medical context and a multiple choice question related to it, select the correct answers from the four options. The answer is binary string with length is the number of opitons, 1 is correct and 0 is incorrect",
+    #     load_8bit=True,
+    #     base_model="medalpaca/medalpaca-7b",
+    #     lora_weights="./lora-alpaca/checkpoint-800",
+    # )
