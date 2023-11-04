e8fafefe150f2a10182588032a30c681f979afbf
trieulv <trieulv@ftech.ai>
2023-11-04 16:56:53 +0000

    chore:

--- bash_scripts/finetune.sh
+++ bash_scripts/finetune.sh
@@ -1,10 +1,13 @@
 python finetune.py \
     --base_model 'medalpaca/medalpaca-7b' \
-    --data_path 'medalpaca/medical_meadow_mediqa' \
+    --data_path 'medalpaca/medical_meadow_medqa' \
+    --instruction_key 'instruction' \
+    --input_key 'input' \
+    --output_key 'output' \
     --output_dir './lora-alpaca' \
     --batch_size 128 \
     --micro_batch_size 4 \
-    --num_epochs 3 \
+    --num_epochs 1 \
     --learning_rate 1e-4 \
     --cutoff_len 512 \
     --val_set_size 2000 \
@@ -13,4 +16,5 @@ python finetune.py \
     --lora_dropout 0.05 \
     --lora_target_modules '[q_proj,v_proj]' \
     --train_on_inputs \
-    --group_by_length
+    --group_by_length \
+    # --resume_from_checkpoint ./lora-alpaca/checkpoint-200
